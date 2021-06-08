282c9d7b66729b7efcb9c5a89ef54162bab81974
YogPanjarale <yog.panjarale@gmail.com>
2021-06-08 22:09:27 +0530

    sun and background

--- main.js
+++ main.js
@@ -22,3 +22,38 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scence, camera);
+
+//Lights 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, 0, 0);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scence.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scence.add(lightHelper, gridHelper);
+
+//Background image
+const spaceTexture = new THREE.TextureLoader().load("2k_stars_milky_way.jpg");
+scence.background = spaceTexture;
+
+//sun
+const sunTexture = new THREE.TextureLoader().load('8k_sun.jpg')
+const sun = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial({
+    map:sunTexture
+  })
+)
+
+scence.add(sun)
+const controls = new OrbitControls(camera, renderer.domElement);
+console.log(controls.center)
+function animate() {
+	requestAnimationFrame(animate);
+  controls.update()
+	renderer.render(scence, camera);
+}
+
+animate();
